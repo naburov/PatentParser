bd9f99ec53c73f4b58fe986528435dab9ccec669
naburov <burov.nikita42@gmail.com>
2019-11-05 21:40:12 +0500

    React подключен

--- PP3/Startup.cs
+++ PP3/Startup.cs
@@ -12,6 +12,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using PP3.Models;
 
+using JavaScriptEngineSwitcher.ChakraCore;
+using JavaScriptEngineSwitcher.Extensions.MsDependencyInjection;
+using React.AspNet;
+
 namespace PP3
 {
     public class Startup
@@ -24,7 +28,7 @@ namespace PP3
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
             services.Configure<CookiePolicyOptions>(options =>
             {
@@ -33,10 +37,18 @@ namespace PP3
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            services.AddJsEngineSwitcher(options => options.DefaultEngineName = ChakraCoreJsEngine.EngineName)
+                .AddChakraCore();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddReact();
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddMvc();
+
             services.AddEntityFrameworkNpgsql().AddDbContext<PatentDbContext>(opt =>
                 opt.UseNpgsql(Configuration.GetConnectionString("MyWebApiConection")));
+
+            return services.BuildServiceProvider();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -51,8 +63,28 @@ namespace PP3
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            app.UseReact(config =>
+            {
+                // If you want to use server-side rendering of React components,
+                // add all the necessary JavaScript files here. This includes
+                // your components as well as all of their dependencies.
+                // See http://reactjs.net/ for more information. Example:
+                //config
+                //  .AddScript("~/js/First.jsx")
+                //  .AddScript("~/js/Second.jsx");
+
+                // If you use an external build too (for example, Babel, Webpack,
+                // Browserify or Gulp), you can improve performance by disabling
+                // ReactJS.NET's version of Babel and loading the pre-transpiled
+                // scripts. Example:
+                //config
+                //  .SetLoadBabel(false)
+                //  .AddScriptWithoutTransform("~/js/bundle.server.js");
+            });
+
             app.UseStaticFiles();
+            app.UseDefaultFiles();
+            app.UseHttpsRedirection();            
             app.UseCookiePolicy();
 
             app.UseMvc(routes =>
